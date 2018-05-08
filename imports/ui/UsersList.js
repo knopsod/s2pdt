0a2511411b93d4b27d10995fab4a3702fb44bca4
Noppadon Sodram <knopsod@gmail.com>
2018-05-09 04:41:49 +0700

    Subscribe all users data

--- imports/ui/UsersList.js
+++ imports/ui/UsersList.js
@@ -8,16 +8,32 @@ class UsersList extends React.Component {
   constructor(props) {
     super(props)
   }
+  renderUsers() {
+    return (
+      <ul>
+        {this.props.users.map(user => <li key={user._id}>{user.emails[0].address}</li>)}
+      </ul>
+    );
+  }
   render() {
     return (
       <div>
         <PrivateHeader title="Users"/>
         <div className="page-content">
           Users page content.
+          <div>
+            {this.renderUsers()}
+          </div>
         </div>
       </div>
     );
   }
 };
 
-export default UsersList;
+export default createContainer(() => {
+  Meteor.subscribe('allUsers');
+  console.log(Meteor.users.find().fetch());
+  return {
+    users: Meteor.users.find().fetch()
+  }
+}, UsersList);
