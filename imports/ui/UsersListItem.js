e4e1a07be5254b259248ef2e531449618b8b8385
Noppadon Sodram <knopsod@gmail.com>
2018-05-10 05:19:50 +0700

    Edit setting users role

--- imports/ui/UsersListItem.js
+++ imports/ui/UsersListItem.js
@@ -6,7 +6,7 @@ class UsersListItem extends React.Component {
   constructor(props) {
     super(props);
   }
-  handleRoleClick(role) {
+  handleRoleClick(_id, role) {
     this.props.meteorCall('users.role_update',
       Meteor.userId(),
       { role },
@@ -20,9 +20,9 @@ class UsersListItem extends React.Component {
       {(this.props.user.role === 1)?'admin':undefined}
       {(this.props.user.role === 2)?'user':undefined}
       {(this.props.user.role === 3)?'customer':undefined}
-      <button onClick={this.handleRoleClick.bind(this, 1)}>Admin</button>
-      <button onClick={this.handleRoleClick.bind(this, 2)}>User</button>
-      <button onClick={this.handleRoleClick.bind(this, 3)}>Customer</button>
+      <button onClick={this.handleRoleClick.bind(this, this.props.user._id, 1)}>Admin</button>
+      <button onClick={this.handleRoleClick.bind(this, this.props.user._id, 2)}>User</button>
+      <button onClick={this.handleRoleClick.bind(this, this.props.user._id, 3)}>Customer</button>
     </li>
   }
 }
