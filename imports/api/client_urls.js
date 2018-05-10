c443e92904043bcaa48670e34f40bb20e37d55c4
Noppadon Sodram <knopsod@gmail.com>
2018-05-10 07:02:53 +0700

    Add schema for check correct url in imports/api/client_urls.js

--- imports/api/client_urls.js
+++ imports/api/client_urls.js
@@ -19,6 +19,13 @@ Meteor.methods({
 
     const { url } = client_url;
 
+    new SimpleSchema({
+      url: {
+        type: String,
+        regEx: SimpleSchema.RegEx.Url
+      }
+    }).validate({ url });
+
     return ClientUrls.insert({
       url,
       userId: this.userId,
