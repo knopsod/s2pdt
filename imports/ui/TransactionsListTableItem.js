f88d13101b65f22dfbd920fdb3fd57ac239660b3
Noppadon Sodram <knopsod@gmail.com>
2018-10-09 03:15:09 +0700

    trace separate use * instead -

--- imports/ui/TransactionsListTableItem.js
+++ imports/ui/TransactionsListTableItem.js
@@ -71,7 +71,7 @@ class TransactionsListTableItem extends Component {
       <td className="text-center"> { tran.transfer_detail } </td>
       <td className="text-center" style={{backgroundColor: statusBgColor, color: 'gray'}}> { tran.isApproved ? 'Approved' : 'None Approve' }  </td>
       <td className="text-center">{ tran.client_transaction_id }</td>
-      <td className="text-center">{ tran.client_url }-{ tran.approver }-{ tran.owner }-{ tran.short_client_url }</td>
+      <td className="text-center">{ tran.client_url }*{ tran.approver }*{ tran.owner }*{ tran.short_client_url }</td>
     </tr>;
   }
 }
